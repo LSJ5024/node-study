3c0cf8ebc55e2c0c528b341b2af7786a1a4f047e
LSJ <asd5818664@gmail.com>
2025-12-12 15:29:35 +0900

    Sequelize, Mysql 실습

--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../model/db');
+
 
 router.get("/", function (req, res) {
-  let page = req.query.page;
-  console.log("[QUERY]");
-  console.log(page);
-
-  res.send({"key":"value"});
+    res.render('index', {title:"EJS 메인페이지"});
 })
 
 router.get("/about", function (req, res){
@@ -18,5 +16,48 @@ router.post("/postapi", function (req, res){
   console.log(body);
   res.send("This is post API page");
 })
+
+
+
+/*
+    C: Create
+    R: Read
+    U: Update
+    D: Delete
+*/
+
+
+
+router.get("/data/create",function(req,res){
+    let user_id = parseInt(Math.random() * 10000)
+    db.users.create({user_id:user_id}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+
+router.get("/data/read",function(req,res){
+    db.users.findAll().then(function(result){
+        res.send({success:200, data:result})
+    })
+})
+
+router.post("/data/update",function(req,res){
+    let target_id = req.body.target_id;
+    db.users.update({user_id:9999},{where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    }
+    )
+})
+
+
+router.post("/data/delete",function(req,res){
+    let target_id = req.body.target_id;
+    db.users.destroy({where:{user_id:target_id}}).then(function(result){
+        res.send({success:200})
+    })
+})
+
+
 
 module.exports = router;
